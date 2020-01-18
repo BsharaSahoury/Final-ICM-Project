b0498e6e8391aa5bd2445eda82ad8a8388ee5764
samishekh <samisnow97@gmail.com>
2020-01-19 00:00:58 +0200

    sdvsvd

--- ICM/src/DBconnection/mysqlConnection.java
+++ ICM/src/DBconnection/mysqlConnection.java
@@ -2493,7 +2493,7 @@ public class mysqlConnection {
 			}
 			if (start != null) {
 				stm2 = con.prepareStatement(
-						"UPDATE requestinphase SET start_date=?,due_date=? WHERE request_id=? AND phase=? AND repetion=?;");
+						"UPDATE requestinphase SET start_date=? WHERE request_id=? AND phase=? AND repetion=?;");
 				Date st = Date.valueOf(start);
 				long s = st.getTime() + (int) (1000 * 60 * 60 * 24);
 				st = new java.sql.Date(s);
